c66a12dee642abfa7701d794ae2d554c82b132d9
SaidganiUsm <saidgani110@gmail.com>
2024-01-02 22:42:08 +0500

    [3] Identity service

---
+++ src/OnlineAppointmentSchedulingSystem.Infrastructure/Identity/EmailService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using OnlineAppointmentSchedulingSystem.Application.Common.Interfaces;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace OnlineAppointmentSchedulingSystem.Infrastructure.Identity
+{
+	public class EmailService : IEmailService
+	{
+		private readonly IConfiguration _configuration;
+
+		public EmailService(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public async Task SendEmailAsync(string toEmail, string subject, string content)
+		{
+			var apiKey = _configuration["EmailSender"];
+			var client = new SendGridClient(apiKey);
+			var from = new EmailAddress(_configuration["EmailSenderSendGrid"], "OnlineAppSystem");
+			var to = new EmailAddress(toEmail);
+			var htmlContent = $"<strong>{content}</strong>";
+			var msg = MailHelper.CreateSingleEmail(from, to, subject, content, htmlContent);
+			var response = await client.SendEmailAsync(msg);
+		}
+	}
+}
--- src/OnlineAppointmentSchedulingSystem.Infrastructure/Identity/IdentityService.cs
+++ src/OnlineAppointmentSchedulingSystem.Infrastructure/Identity/IdentityService.cs
@@ -1,43 +1,348 @@
-﻿using OnlineAppointmentSchedulingSystem.Application.Common.Interfaces;
+﻿using Microsoft.AspNetCore.Builder.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using OnlineAppointmentSchedulingSystem.Application.Common.Interfaces;
 using OnlineAppointmentSchedulingSystem.Application.Common.Models.Account;
+using OnlineAppointmentSchedulingSystem.Core.Entities;
+using OnlineAppointmentSchedulingSystem.Infrastructure.Common.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace OnlineAppointmentSchedulingSystem.Infrastructure.Identity
 {
 	public class IdentityService : IIdentityService
 	{
-		public Task<LoginResponse> AssignRoleToUserAsync(string role)
+		private readonly UserManager<User> _userManager;
+		private readonly SignInManager<User> _signInManager;
+		private readonly ILogger<IdentityService> _logger;
+		private readonly IEmailService _emailService;
+		private readonly RoleManager<Role> _roleManager;
+		private readonly AuthSettingsOptions _authSettingsOptions;
+		private readonly AppOptions _appOptions;
+		private readonly ICurrentUserService _currentUserService;
+
+		public IdentityService(
+			UserManager<User> userManager,
+			SignInManager<User> signInManager,
+			ILogger<IdentityService> logger,
+			IEmailService emailService,
+			RoleManager<Role> roleManager,
+			IOptions<AuthSettingsOptions> authSettingsOptions,
+			IOptions<AppOptions> appOptions,
+			ICurrentUserService currentUserService
+		)
 		{
-			throw new NotImplementedException();
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_logger = logger;
+			_emailService = emailService;
+			_roleManager = roleManager;
+			_authSettingsOptions = authSettingsOptions.Value;
+			_appOptions = appOptions.Value;
+			_currentUserService = currentUserService;
 		}
 
-		public Task<ChangePasswordResponse> ChangeUserPasswordAsync(string email, ChangePasswordViewModel model)
+		public async Task<LoginResponse> LoginUserAsync(LoginViewModel userModel)
 		{
-			throw new NotImplementedException();
+			if (
+				userModel is null
+				|| string.IsNullOrEmpty(userModel.Email)
+				|| string.IsNullOrEmpty(userModel.Password)
+			)
+			{
+				return new LoginResponse
+				{
+					Errors = new[] { "User data is emtpy" },
+					IsSuccess = false,
+				};
+			}
+			var user = await _userManager.FindByEmailAsync(userModel.Email);
+
+			if (user is null)
+			{
+				return new LoginResponse
+				{
+					Errors = new[] { "User is not found" },
+					IsSuccess = false,
+				};
+			}
+
+			var result = await _signInManager.PasswordSignInAsync(
+				user,
+				userModel.Password,
+				false,
+				false
+			);
+
+			if (result.Succeeded)
+			{
+				_logger.LogInformation("User logged in");
+			}
+			else
+			{
+				return new LoginResponse
+				{
+					Errors = new[] { "Wrong password or email" },
+					IsSuccess = false,
+				};
+			}
+
+			if (!user.EmailConfirmed)
+			{
+				return new LoginResponse
+				{
+					Errors = new[] { "User is not active" },
+					IsSuccess = false,
+				};
+			}
+
+			var token = await GenerateJWTTokenWithUserClaimsAsync(user);
+
+			token.Role = (await _userManager.GetRolesAsync(user)).FirstOrDefault()!;
+			token.UserId = user.Id;
+
+			return new LoginResponse { IsSuccess = true, Result = token };
 		}
 
-		public Task<RegisterResponse> ConfirmUserEmailAsync(string userId, string token)
+		/// <summary>
+		/// Generation of JWT token with User claims including Email and role
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns></returns>
+		private async Task<TokenModel> GenerateJWTTokenWithUserClaimsAsync(User user)
 		{
-			throw new NotImplementedException();
+			var roles = await _userManager.GetRolesAsync(user);
+			var claims = new List<Claim> { new Claim(ClaimTypes.Email, user.Email!), };
+
+			foreach (var role in roles)
+			{
+				claims.Add(new Claim(ClaimTypes.Role, role));
+			}
+
+			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authSettingsOptions.Key));
+
+			var token = new JwtSecurityToken(
+				issuer: _authSettingsOptions.Issuer,
+				audience: _authSettingsOptions.Audience,
+				claims: claims,
+				expires: DateTime.Now.AddDays(1),
+				signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+			);
+
+			string tokenAsString = new JwtSecurityTokenHandler().WriteToken(token);
+
+			return new TokenModel { AccessToken = tokenAsString, ExpireDate = token.ValidTo };
 		}
 
-		public Task<ResetPasswordResponse> ForgetPasswordAsync(string email)
+		public async Task<ResetPasswordResponse> ForgetPasswordAsync(string email)
 		{
-			throw new NotImplementedException();
+			var user = await _userManager.FindByEmailAsync(email);
+			if (user is null)
+			{
+				return new ResetPasswordResponse
+				{
+					IsSuccess = false,
+					Message = "No user associated with email"
+				};
+			}
+
+			var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+			var encodedToken = Encoding.UTF8.GetBytes(token);
+			var validToken = WebEncoders.Base64UrlEncode(encodedToken);
+
+			string url =
+				$"{_appOptions.ClientApp}/auth/reset-password?email={email}&token={validToken}";
+
+			await _emailService.SendEmailAsync(
+				email,
+				"Reset Password",
+				"<h1>Follow the instructions to reset your password</h1>"
+					+ $"<p>To reset your password <a href='{url}'>Click here</a></p>"
+			);
+
+			return new ResetPasswordResponse
+			{
+				IsSuccess = true,
+				Message = "Reset password URL has been sent to the email successfully"
+			};
 		}
 
-		public Task<LoginResponse> LoginUserAsync(LoginViewModel userModel)
+		public async Task<ResetPasswordResponse> ResetPasswordAsync(ResetPasswordViewModel model)
 		{
-			throw new NotImplementedException();
+			var user = await _userManager.FindByEmailAsync(model.Email);
+			if (user is null)
+			{
+				return new ResetPasswordResponse
+				{
+					IsSuccess = false,
+					Message = "No user associated with email"
+				};
+			}
+
+			if (model.NewPassword != model.ConfirmPassword)
+				return new ResetPasswordResponse
+				{
+					IsSuccess = false,
+					Message = "Password does not match its confirmation"
+				};
+
+			var decodedToken = WebEncoders.Base64UrlDecode(model.Token);
+			string normalToken = Encoding.UTF8.GetString(decodedToken);
+
+			var result = await _userManager.ResetPasswordAsync(
+				user,
+				normalToken,
+				model.NewPassword
+			);
+
+			if (result.Succeeded)
+				return new ResetPasswordResponse
+				{
+					Message = "Password has been reset successfully!",
+					IsSuccess = true
+				};
+
+			return new ResetPasswordResponse
+			{
+				Message = "Something went wrong",
+				IsSuccess = false,
+				Errors = result.Errors.Select(e => e.Description)
+			};
 		}
 
-		public Task<RegisterResponse> RegisterUserAsync(RegisterViewModel model)
+		public async Task<RegisterResponse> RegisterUserAsync(RegisterViewModel model)
 		{
-			throw new NotImplementedException();
+			if (model is null)
+				throw new NullReferenceException("Register Model is null")!;
+
+			if (model.Password != model.ConfirmPassword)
+				return new RegisterResponse
+				{
+					Message = "Confirm password doesn't match the password",
+					IsSuccess = false,
+				};
+
+			var user = new User { Email = model.Email, UserName = model.Email, };
+			var result = await _userManager.CreateAsync(user, model.Password);
+
+			if (result.Succeeded)
+			{
+				var confirmEmailToken = await _userManager.GenerateEmailConfirmationTokenAsync(
+					user
+				);
+
+				var encodedEmailToken = Encoding.UTF8.GetBytes(confirmEmailToken);
+				var validEmailToken = WebEncoders.Base64UrlEncode(encodedEmailToken);
+
+				var url =
+					$"{_appOptions.Url}/api/auth/confirm-email?userid={user.Id}&token={validEmailToken}";
+
+				await _emailService.SendEmailAsync(
+					user.Email,
+					"Confirm your email",
+					$"<h1>Welcome to Auctionify</h1>"
+						+ $"<p>Please confirm your email by <a href='{url}'>clicking here</a></p>"
+				);
+
+				return new RegisterResponse
+				{
+					Message = "User created successfully!",
+					IsSuccess = true,
+				};
+			}
+
+			return new RegisterResponse
+			{
+				Message = "User was not created",
+				IsSuccess = false,
+				Errors = result.Errors.Select(e => e.Description),
+			};
 		}
 
-		public Task<ResetPasswordResponse> ResetPasswordAsync(ResetPasswordViewModel model)
+		public async Task<RegisterResponse> ConfirmUserEmailAsync(string userId, string token)
 		{
-			throw new NotImplementedException();
+			var user = await _userManager.FindByIdAsync(userId);
+			if (user is null)
+				return new RegisterResponse { IsSuccess = false, Message = "User not found", };
+
+			var decodedToken = WebEncoders.Base64UrlDecode(token);
+			string normalToken = Encoding.UTF8.GetString(decodedToken);
+
+			var result = await _userManager.ConfirmEmailAsync(user, normalToken);
+
+			if (result.Succeeded)
+				return new RegisterResponse
+				{
+					Message = "Email confirmed successfully!",
+					IsSuccess = true,
+				};
+
+			return new RegisterResponse
+			{
+				Message = "Email did not confirm",
+				IsSuccess = false,
+				Errors = result.Errors.Select(e => e.Description),
+			};
+		}
+
+		public async Task<ChangePasswordResponse> ChangeUserPasswordAsync(string email, ChangePasswordViewModel model)
+		{
+			var user = await _userManager.FindByEmailAsync(email);
+
+			if (user is null)
+			{
+				return new ChangePasswordResponse
+				{
+					IsSuccess = false,
+					Message = "No user associated with the provided email"
+				};
+			}
+
+			var isOldPasswordValid = await _userManager.CheckPasswordAsync(user, model.OldPassword);
+			if (!isOldPasswordValid)
+			{
+				return new ChangePasswordResponse
+				{
+					IsSuccess = false,
+					Message = "Invalid old password"
+				};
+			}
+
+			if (model.NewPassword != model.ConfirmNewPassword)
+			{
+				return new ChangePasswordResponse
+				{
+					IsSuccess = false,
+					Message = "New password does not match its confirmation"
+				};
+			}
+
+			var result = await _userManager.ChangePasswordAsync(
+				user,
+				model.OldPassword,
+				model.NewPassword
+			);
+
+			if (result.Succeeded)
+			{
+				return new ChangePasswordResponse
+				{
+					IsSuccess = true,
+					Message = "Password has been changed successfully!"
+				};
+			}
+
+			return new ChangePasswordResponse
+			{
+				IsSuccess = false,
+				Message = "Something went wrong",
+				Errors = result.Errors.Select(e => e.Description)
+			};
 		}
 	}
 }
