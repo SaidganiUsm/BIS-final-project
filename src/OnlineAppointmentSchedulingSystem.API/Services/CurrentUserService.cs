a132a30e4de9aee5cd255c59e3ec2c1bf76e1aad
SaidganiUsm <saidgani110@gmail.com>
2024-01-02 22:41:53 +0500

    [3] CurrentUser service

---
+++ src/OnlineAppointmentSchedulingSystem.API/Services/CurrentUserService.cs
@@ -0,0 +1,17 @@
+﻿using OnlineAppointmentSchedulingSystem.Application.Common.Interfaces;
+using System.Security.Claims;
+
+namespace OnlineAppointmentSchedulingSystem.API.Services
+{
+	public class CurrentUserService : ICurrentUserService
+	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+
+		public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+		{
+			_httpContextAccessor = httpContextAccessor;
+		}
+
+		public string? UserEmail => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Email);
+	}
+}
