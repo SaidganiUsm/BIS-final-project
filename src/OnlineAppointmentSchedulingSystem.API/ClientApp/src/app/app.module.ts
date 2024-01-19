ad9d645de51c5dacaf6b12a8590d150001cfa1bc
SaidganiUsm <saidgani110@gmail.com>
2024-01-19 21:14:47 +0500

    [4] Guards for cheking role and interceptors with routing modules

--- src/OnlineAppointmentSchedulingSystem.API/ClientApp/src/app/app.module.ts
+++ src/OnlineAppointmentSchedulingSystem.API/ClientApp/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiAuthorizationModule } from './api-authorization/api-authorization.module';
+import { AuthorizeInterceptor } from './api-authorization/authorize.interceptor';
+import { GeneralModule } from './components/general/general.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ApiAuthorizationModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        GeneralModule,
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthorizeInterceptor,
+            multi: true,
+        },
+    ],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
