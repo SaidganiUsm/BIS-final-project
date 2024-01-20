76362e92ecd27c3402ee717e127927c271b3f3d1
SaidganiUsm <saidgani110@gmail.com>
2024-01-20 13:24:35 +0500

    [6] Dto for getting a list of items

---
+++ src/OnlineAppointmentSchedulingSystem.Application/Common/DTOs/GetListDto.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineAppointmentSchedulingSystem.Application.Common.DTOs
+{
+	public class GetListDto<T> : BasePageableModel
+	{
+		public IList<T> Items
+		{
+			get => _items ??= new List<T>();
+			set => _items = value;
+		}
+
+		private IList<T>? _items;
+	}
+}
