fe776f6dc85a921f92e05657056c7ed311074d94
SaidganiUsm <saidgani110@gmail.com>
2024-01-20 13:25:10 +0500

    [6] Functionality for fetching appointments of the one user

---
+++ src/OnlineAppointmentSchedulingSystem.Application/Features/Appointments/Queries/GetUsersAppointments/GetUsersAppointmentsQuery.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using OnlineAppointmentSchedulingSystem.Application.Features.Appointments.Queries.GetUserAppointments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAppointmentSchedulingSystem.Application.Features.Appointments.Queries.GetUsersAppointments
+{
+	public class GetUsersAppointmentsQuery : IRequest<GetUsersAppointmentsResponse>
+	{
+	}
+
+	public class GetUsersAppointmentsQueryHandler
+		: IRequestHandler<GetUsersAppointmentsQuery, GetUsersAppointmentsResponse>()
+	{
+		public Task<GetUsersAppointmentsResponse> Handle(GetUsersAppointmentsQuery request, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
