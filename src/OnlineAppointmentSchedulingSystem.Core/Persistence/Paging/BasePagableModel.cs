013bbb6cc2516f474fcd38008eb7477427067c6a
SaidganiUsm <saidgani110@gmail.com>
2024-01-20 13:24:05 +0500

    [6] Base pagination models and logic

---
+++ src/OnlineAppointmentSchedulingSystem.Core/Persistence/Paging/BasePagableModel.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineAppointmentSchedulingSystem.Core.Persistence.Paging
+{
+	public abstract class BasePagableModel
+	{
+		public int Index { get; set; }
+
+		public int Size { get; set; }
+
+		public int Count { get; set; }
+
+		public int Pages { get; set; }
+
+		public bool HasPrevious { get; set; }
+
+		public bool HasNext { get; set; }
+	}
+}
