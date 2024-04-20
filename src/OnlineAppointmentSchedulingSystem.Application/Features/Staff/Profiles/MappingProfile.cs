489d517e6aa52dc28850b211649fdc3e778fef03
SaidganiUsm <saidgani110@gmail.com>
2024-04-20 17:18:58 +0500

    [6] Mapping profiles update

---
+++ src/OnlineAppointmentSchedulingSystem.Application/Features/Staff/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using OnlineAppointmentSchedulingSystem.Application.Features.Staff.Commands.Create;
+using OnlineAppointmentSchedulingSystem.Core.Entities;
+
+namespace OnlineAppointmentSchedulingSystem.Application.Features.Staff.Profiles
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile() 
+		{
+			CreateMap<Appointment, CreateAppoitnmentByStaffCommandResponse>().ReverseMap();
+		}
+	}
+}
