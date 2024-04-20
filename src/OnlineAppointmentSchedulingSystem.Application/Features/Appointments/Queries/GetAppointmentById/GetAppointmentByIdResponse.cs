bd0a03f78c3d5d79b68d520657cd35bb9dffecd9
SaidganiUsm <saidgani110@gmail.com>
2024-04-20 12:26:46 +0500

    [6] Get appointment by id handler added

---
+++ src/OnlineAppointmentSchedulingSystem.Application/Features/Appointments/Queries/GetAppointmentById/GetAppointmentByIdResponse.cs
@@ -0,0 +1,8 @@
+﻿using OnlineAppointmentSchedulingSystem.Application.Features.Appointments.Queries.BaseQueryModels;
+
+namespace OnlineAppointmentSchedulingSystem.Application.Features.Appointments.Queries.GetAppointmentById
+{
+	public class GetAppointmentByIdResponse : GetAllAppointments
+	{
+	}
+}
