a8704e8f4531eafd38b97a5914b2d58dfbfc3d4b
SaidganiUsm <saidgani110@gmail.com>
2024-04-26 15:21:27 +0500

    [21] Mini top bar about user

--- src/OnlineAppointmentSchedulingSystem.API/ClientApp/src/app/ui-elements/mini-profile-bar/mini-profile-bar.component.ts
+++ src/OnlineAppointmentSchedulingSystem.API/ClientApp/src/app/ui-elements/mini-profile-bar/mini-profile-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { UserDto } from 'src/app/models/Appointments/appointment-model';
+import { UserDto } from 'src/app/models/DTOs/UserDto-model';
 
 @Component({
     selector: 'app-mini-profile-bar',
