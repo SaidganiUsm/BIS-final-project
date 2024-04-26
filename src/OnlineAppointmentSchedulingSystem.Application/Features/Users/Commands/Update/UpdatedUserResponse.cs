d5fd447c28529a6402051c8ec289b133741f6d2d
SaidganiUsm <saidgani110@gmail.com>
2024-04-26 15:21:59 +0500

    [21] Update user data back-end

---
+++ src/OnlineAppointmentSchedulingSystem.Application/Features/Users/Commands/Update/UpdatedUserResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineAppointmentSchedulingSystem.Application.Features.Users.Commands.Update
+{
+	internal class UpdatedUserResponse
+	{
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string Email { get; set; }
+
+		public string PhoneNumber { get; set; }
+
+		public string AboutMe { get; set; }
+	}
+}
